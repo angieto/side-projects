f66793718da46311488f86ddab969177f0fe7dc0
Angie To <angie.to@berkeley.edu>
2021-10-04 21:43:46 -0700

    completed drum kit UI

---
+++ drumkit/app.js
@@ -0,0 +1,12 @@
+;(function () {
+    "use strict"
+
+    // Variables
+    const playingClass = "playing"
+
+    // DOM Elements
+    const crashRide = document.querySelector("#crash-ride")
+    const hiHatTop = document.querySelector("#hihat-top")
+
+    // Event listeners
+})()
